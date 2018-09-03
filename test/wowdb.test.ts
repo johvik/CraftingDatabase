a3a3523fa673c65a09ad35382ae81dd53a3e972c
johvik <johvik@users.noreply.github.com>
2018-09-03 17:46:03 +0200

    More tslint options

--- test/wowdb.test.ts
+++ test/wowdb.test.ts
@@ -4,18 +4,18 @@ import fs from "fs";
 import path from "path";
 
 describe("parseRecipesPage", () => {
-    it("should parse the first page", async () => {
+    it("should parse the first page", () => {
         const dom = new JSDOM(fs.readFileSync(path.join(__dirname, "data", "wowdb_recipies_page1.html.data")).toString());
-        const result = await parseRecipesPage(dom.window.document);
+        const result = parseRecipesPage(dom.window.document);
         expect(result.spellIds).toHaveLength(200);
         expect(result.spellIds[0]).toEqual(252378);
         expect(result.spellIds[199]).toEqual(269529);
         expect(result.next).toEqual(true);
     });
 
-    it("should parse the last page", async () => {
+    it("should parse the last page", () => {
         const dom = new JSDOM(fs.readFileSync(path.join(__dirname, "data", "wowdb_recipies_page3.html.data")).toString());
-        const result = await parseRecipesPage(dom.window.document);
+        const result = parseRecipesPage(dom.window.document);
         expect(result.spellIds).toHaveLength(194);
         expect(result.spellIds[0]).toEqual(256510);
         expect(result.spellIds[193]).toEqual(272230);
