43c7d4fde501c8b2a510e8b9f504120eb26c16ab
johvik <johvik@users.noreply.github.com>
2018-09-09 12:38:44 +0200

    Auctions.items method

--- src/service/auctions.ts
+++ src/service/auctions.ts
@@ -67,4 +67,12 @@ export class Auctions {
             }
         }
     }
+
+    static async items(): Promise<Set<number>> {
+        const ids = await getRepository(Auction)
+            .createQueryBuilder("auction")
+            .select("DISTINCT auction.id")
+            .getRawMany();
+        return new Set<number>(ids.map(id => id.id));
+    }
 }
